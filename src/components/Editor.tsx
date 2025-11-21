377e057dfc64a6b88d5701cc9081e5308e66a00f
hiroyuki_ishige <hiroyukiishige@hotmail.com>
2025-11-21 07:07:41 +0700

    Fix BlockNote import error and restore popover component
    - Restore popover.tsx component that was previously removed
    - Fix incorrect BlockNote locale import in Editor.tsx
    - Remove problematic dictionary configuration from useCreateBlockNote
    - Update package dependencies (package.json and package-lock.json)

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,3 @@
-import { en } from "@blocknote/core/locales";
 import { useCreateBlockNote } from "@blocknote/react";
 import "@blocknote/mantine/style.css";
 import { BlockNoteView } from "@blocknote/mantine";
@@ -13,8 +12,6 @@ function Editor({ onChange, initialContent }: EditorProps) {
   //const [value, setValue] = useState(initialContent || "");
 
   const editor = useCreateBlockNote({
-    
-    dictionary: en, //set menu "Heading", "Paragraph", etc to English(en). If you want to use Japanese, change "en" to "ja".
     initialContent:
       initialContent != null ? JSON.parse(initialContent) : undefined,
   });
